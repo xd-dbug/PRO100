42def564f33bca3a65a3e99d66ddf587eda4f665
JACE062 <123290590+JACE062@users.noreply.github.com>
2025-08-14 10:26:48 -0600

    Fixed JavaFX and added UI frame

---
+++ src/main/java/csc180/damian/swiney/workplace_incident_report_system/ReportsPageController.java
@@ -0,0 +1,12 @@
+package csc180.damian.swiney.workplace_incident_report_system;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ReportsPageController {
+    @FXML
+    public Label ReportsPaneText;
+
+
+
+}
