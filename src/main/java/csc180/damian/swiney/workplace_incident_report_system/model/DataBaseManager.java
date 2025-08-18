c066fd7be51d7364bad983dda6f869692450a280
XPenguinGodX <amadeo.2001@gmail.com>
2025-08-18 16:03:35 -0600

    Phase 1 debugging concluding
    Hopefully this works i am recreating our docker sql instance and connecting it to intellij

--- src/main/java/csc180/damian/swiney/workplace_incident_report_system/model/DataBaseManager.java
+++ src/main/java/csc180/damian/swiney/workplace_incident_report_system/model/DataBaseManager.java
@@ -1,5 +1,12 @@
 package csc180.damian.swiney.workplace_incident_report_system.model;
 
 public class DataBaseManager {
-
+    public DataBaseManager() {
+        String connectionUrl =
+                "jdbc:sqlserver://localhost:1433;"
+                        + "database=WIRMS;"
+                        + "user=sa;"
+                        + "password=WIRMS101!!;"
+                        + "loginTimeout=30;";
+    }
 }
