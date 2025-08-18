44c57acee3d62bebbcaab7c1672303edafca25e7
Damian Swiney <damian.swiney1@gmail.com>
2025-08-18 10:54:30 -0600

    Deleted Runner.java and Employee should show up in the table

--- src/main/java/csc180/damian/swiney/workplace_incident_report_system/model/Employee.java
+++ src/main/java/csc180/damian/swiney/workplace_incident_report_system/model/Employee.java
@@ -1,50 +1,53 @@
 package csc180.damian.swiney.workplace_incident_report_system.model;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Employee {
 
-    private int employeeId;
-    private String firstName;
-    private String lastName;
-    private String department;
+    private SimpleIntegerProperty employeeId;
+    private SimpleStringProperty firstName;
+    private SimpleStringProperty lastName;
+    private SimpleStringProperty department;
 
 
     public Employee(int employeeId, String firstName, String lastName, String department) {
-        this.employeeId = employeeId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.department = department;
+        this.employeeId = new SimpleIntegerProperty(employeeId);
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.department = new SimpleStringProperty(department);
     }
 
     // Getters and Setters
     public int getEmployeeId() {
-        return employeeId;
+        return employeeId.get();
     }
 
     public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
+        this.employeeId.set(employeeId);
     }
 
     public String getFirstName() {
-        return firstName;
+        return firstName.get();
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        this.firstName.set(firstName);
     }
 
     public String getLastName() {
-        return lastName;
+        return lastName.get();
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        this.lastName.set(lastName);
     }
 
     public String getDepartment() {
-        return department;
+        return department.get();
     }
 
     public void setDepartment(String department) {
-        this.department = department;
+        this.department.set(department);
     }
 }
