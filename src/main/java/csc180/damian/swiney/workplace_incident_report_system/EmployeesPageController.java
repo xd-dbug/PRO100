478f5969f1f6c9f7979421d37f1439942f7ea2f9
Damian Swiney <damian.swiney1@gmail.com>
2025-08-19 11:52:09 -0600

    Add Employee autoshow on table and in database

--- src/main/java/csc180/damian/swiney/workplace_incident_report_system/EmployeesPageController.java
+++ src/main/java/csc180/damian/swiney/workplace_incident_report_system/EmployeesPageController.java
@@ -1,5 +1,6 @@
 package csc180.damian.swiney.workplace_incident_report_system;
 
+import csc180.damian.swiney.workplace_incident_report_system.model.DataBaseManager;
 import csc180.damian.swiney.workplace_incident_report_system.model.Employee;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -13,6 +14,8 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.sql.*;
 
 public class EmployeesPageController {
 
@@ -30,19 +33,20 @@ public class EmployeesPageController {
     public TableColumn<Employee, String> departmentColumn;
 
     private ObservableList<Employee> employees = FXCollections.observableArrayList();
+    private final DataBaseManager db = new DataBaseManager();
 
-    public void addEmployee(Employee employee) {
-        employees.add(employee);
-    }
+
 
     @FXML
-    public void initialize() {
-        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+    public void initialize() throws SQLException {
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("employeeId"));
         firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
         lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
         departmentColumn.setCellValueFactory(new PropertyValueFactory<>("department"));
 
+        loadDataFromDatabase();
         employeeTable.setItems(employees);
+
     }
 
     @FXML
@@ -55,12 +59,23 @@ public class EmployeesPageController {
 
         Stage owner = (Stage) EmployeePaneTitle.getScene().getWindow();
         Stage dialog = new Stage();
-
-
         dialog.setTitle("Add New Employee");
         dialog.initOwner(owner);
         dialog.initModality(Modality.WINDOW_MODAL);
         dialog.setScene(new Scene(root, 300, 400));
         dialog.showAndWait();
     }
+
+
+
+    public void loadDataFromDatabase() throws SQLException {
+        employees.clear();
+        employees.addAll(db.getAllEmployees());
+
+    }
+
+    public void addEmployeeToTable(Employee employee) {
+        employeeTable.getItems().add(employee);
+    }
+
 }
--- src/main/java/csc180/damian/swiney/workplace_incident_report_system/model/DataBaseManager.java
+++ src/main/java/csc180/damian/swiney/workplace_incident_report_system/model/DataBaseManager.java
@@ -1,6 +1,8 @@
 package csc180.damian.swiney.workplace_incident_report_system.model;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataBaseManager {
     private static final String HOST = System.getenv().getOrDefault("DB_HOST", "127.0.0.1");
@@ -34,6 +36,53 @@ public class DataBaseManager {
         return false;
 
     }
+
+    public Employee addEmployee(String firstname, String lastname, String department) throws SQLException {
+        String sql = "INSERT INTO employees VALUES (?,?,?)";
+
+        try(Connection conn = connect(); PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            stmt.setString(1, firstname);
+            stmt.setString(2, lastname);
+            stmt.setString(3, department);
+
+            int rowsInserted = stmt.executeUpdate();
+
+            if (rowsInserted > 0) {
+                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int id = generatedKeys.getInt(1);
+                        return new Employee(id, firstname, lastname, department);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<Employee> getAllEmployees() throws SQLException {
+        List<Employee> employees = new ArrayList<>();
+        String sql = "SELECT * FROM employees";
+        try{
+            connect();
+            PreparedStatement stmt = connect().prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("employeeID");
+                String firstname = rs.getString("FirstName");
+                String lastname = rs.getString("LastName");
+                String department = rs.getString("Department");
+
+                employees.add(new Employee(id, firstname, lastname, department));
+            }
+
+        }catch (SQLException e){
+            System.out.println("Unable to get all employees");
+        }
+        return employees;
+    }
+
+
 
 
 
