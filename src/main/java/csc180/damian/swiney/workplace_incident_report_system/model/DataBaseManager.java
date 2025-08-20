3cca82ec0046316391537445db868673bacd602d
XPenguinGodX <amadeo.2001@gmail.com>
2025-08-20 09:02:16 -0600

    sql Methods
    addToMainMethod that sets the reportID, and an add to Injury table that uses the reportID set in the main table as its primary id

--- src/main/java/csc180/damian/swiney/workplace_incident_report_system/model/DataBaseManager.java
+++ src/main/java/csc180/damian/swiney/workplace_incident_report_system/model/DataBaseManager.java
@@ -5,9 +5,10 @@ import java.sql.*;
 public class DataBaseManager {
     private static final String HOST = System.getenv().getOrDefault("DB_HOST", "127.0.0.1");
     private static final String PORT = System.getenv().getOrDefault("DB_PORT", "11433"); // your compose port
-    private static final String DB   = System.getenv().getOrDefault("DB_NAME", "WIRMS");
+    private static final String DB = System.getenv().getOrDefault("DB_NAME", "WIRMS");
     private static final String USER = System.getenv().getOrDefault("DB_USER", "sa");         // or wirms_user
     private static final String PASS = System.getenv().getOrDefault("DB_PASSWORD", "WIRMS101!!"); // or WirmsStrong!1
+    static int reportID;
 
     // SQL Server JDBC URL uses semicolons for properties (not ?& style)
     private static final String URL = String.format(
@@ -15,26 +16,70 @@ public class DataBaseManager {
             HOST, PORT, DB
     );
 
-    /** Open a new connection. Remember to close it in a try-with-resources. */
+    /**
+     * Open a new connection. Remember to close it in a try-with-resources.
+     */
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
 
-    public static boolean verifyUser(String username, String password){
+    public static boolean verifyUser(String username, String password) {
         String sql = "SELECT * FROM users WHERE Username=? AND Password=?";
-        try{
+        try {
             PreparedStatement stmt = connect().prepareStatement(sql);
             stmt.setString(1, username);
             stmt.setString(2, password);
             ResultSet rs = stmt.executeQuery();
             return rs.next();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Unable to verify user please check login information");
         }
         return false;
 
     }
 
+    public static void addReport() {
+
+    }
+
+    public static void addInjury(String description, boolean Hospitalized, String InjuryType) {
+        String InjurySql = "INSERT INTO Injury (InjuryID, Hospitalized, InjuryType) VALUES (?,?,?)";
+
+        addToMainTable(description);
+//
+
+        try {
+            PreparedStatement injuryStmt = connect().prepareStatement(InjurySql);
+            injuryStmt.setInt(1, reportID);
+            injuryStmt.setBoolean(2, Hospitalized);
+            injuryStmt.setString(3, InjuryType);
+            injuryStmt.executeUpdate();
+
+
+        } catch (SQLException e) {
+            System.out.println("Unable to add Injury");
+        }
+
+
+    }
+
+    public static void addToMainTable(String description) {
+
+        try {
+            String MainSql = "INSERT INTO MainTable(Description) VALUES (?)";
+            PreparedStatement stmt = connect().prepareStatement(MainSql);
+            stmt.setString(1, description);
+            stmt.executeUpdate();
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                reportID = rs.getInt(1);
+            } else {
+                throw new SQLException("Unable to get ReportID");
+            }
+        } catch (SQLException e) {
+            System.out.println("Unable to add To MainTable");
+        }
+    }
 
 
 }
